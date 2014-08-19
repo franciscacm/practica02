6621bd56efe1831f63482b937c5be139e3273e58
franciscacm <fcolorad@hotmail.com>
2014-08-19 09:39:07 -0500

    practica02uno
    primera subida que vibra y hace beep

---
+++ js/acciones.js
@@ -0,0 +1,14 @@
+//JavaScript
+	
+$(document).ready(function(e){	//esperar que el documento este ready 
+	document.addEventListener("deviceready",function(){
+		 //espera que el dispositivo este listo para empezar a escuchar los eventos los atrapemos para hacer algo
+		 //para los dispositivos existe el evento de tap que se encuentra en la libreria jquery.mobile
+       $('#beep').tap(function(){
+		   navigator.notification.beep(1);
+	   }); //tap beep
+	   $('#vibrar').tap(function(){
+		   navigator.notofocation.vibrate(1000);
+	   }); //tap vibrar
+	},false); //deviceready
+});//ready
